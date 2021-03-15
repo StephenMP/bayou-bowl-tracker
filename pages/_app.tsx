3b8d00e863de5d28c3ec4a4cf51c753192e10c8b
StephenMP <1.stephen.porter@gmail.com>
2021-03-15 17:30:00 -0600

    Remove CDN link stylesheet

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import type { AppProps /*, AppContext */ } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <>
-    <link
-      rel="stylesheet"
-      href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
-      integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l"
-    />
-    <Component {...pageProps} />
-  </>
+  return <Component {...pageProps} />
 }
 
 export default MyApp
