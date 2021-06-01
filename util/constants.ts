3f370658056888e204175a9c665b4fa45b7c476c
StephenMP <1.stephen.porter@gmail.com>
2021-05-31 18:42:53 -0600

    Fix build

--- util/constants.ts
+++ util/constants.ts
@@ -3,5 +3,5 @@ export const constants = {
     APP_NAME: process.env.NEXT_PUBLIC_APP_NAME!,
     ENVIRONMENT: process.env.NEXT_PUBLIC_APP_ENV!,
     REDIS_URL: process.env.REDIS_URL!,
-    SOCKET_URL: process.env.SOCKET_URL!
+    SOCKET_URL: process.env.NEXT_PUBLIC_SOCKET_URL!
 }
