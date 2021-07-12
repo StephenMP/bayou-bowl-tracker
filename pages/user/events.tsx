41f96772bccb1f788649e83ec6ec550121300fd2
StephenMP <1.stephen.porter@gmail.com>
2021-07-11 23:33:22 -0600

    Only show the events you're registered to

--- pages/user/events.tsx
+++ pages/user/events.tsx
@@ -5,11 +5,13 @@ import CardEvent from "../../components/Cards/CardEvent";
 import Spinner from "../../components/PageChange/Spinner";
 import Admin from "../../layouts/Admin";
 import { useEvents } from "../../lib/swr";
+import { useCurrentUserTeams } from '../../lib/swr/user';
 
 const UserEvents = withPageAuthRequired(() => {
   const { events, isLoading } = useEvents()
+  const { teams, isLoading:teamsLoading } = useCurrentUserTeams({ suspense: false })
 
-  if (isLoading) {
+  if (isLoading || teamsLoading) {
     return (
       <Spinner light={true} />
     )
@@ -17,7 +19,7 @@ const UserEvents = withPageAuthRequired(() => {
 
   return (
     <div className="flex flex-wrap">
-      {events.sort(firstBy('startDate')).filter(e => e.isActive).map((event) =>
+      {events.filter(e => teams.find(t => t.event_id === e.id)).sort(firstBy('startDate')).map((event) =>
         <React.Suspense  key={`suspense-${event.id}`} fallback={<Spinner light={true} />} >
           <div key={event.id} className="w-full lg:w-4/12 px-4">
             <CardEvent event={event} />
