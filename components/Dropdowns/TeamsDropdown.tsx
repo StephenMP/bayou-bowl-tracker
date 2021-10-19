1a0e405c76e7e657b54a8640b130344bbaa78cd7
StephenMP <1.stephen.porter@gmail.com>
2021-10-19 13:58:33 -0600

    Close popdowns, add new socials, change references to monday night hunts

--- components/Dropdowns/TeamsDropdown.tsx
+++ components/Dropdowns/TeamsDropdown.tsx
@@ -1,6 +1,7 @@
 import { createPopper } from "@popperjs/core";
 import Link from "next/link";
 import React from "react";
+import usePopdownCloseEvents from "../../hooks/usePopdownCloseEvents";
 import { routes } from "../../util/routes";
 
 const TeamsDropdown = () => {
@@ -9,19 +10,33 @@ const TeamsDropdown = () => {
   const btnDropdownRef = React.createRef<HTMLAnchorElement>();
   const popoverDropdownRef = React.createRef<HTMLDivElement>();
   const openDropdownPopover = () => {
-    createPopper(btnDropdownRef.current, popoverDropdownRef.current, {
-      placement: "bottom-start",
-    });
-    setDropdownPopoverShow(true);
-  };
-  const closeDropdownPopover = () => {
-    setDropdownPopoverShow(false);
-  };
+    createPopper(btnDropdownRef.current, popoverDropdownRef.current, { placement: "bottom-start", })
+    setDropdownPopoverShow(true)
+  }
+  const closeDropdownPopover = () => setDropdownPopoverShow(false)
+  const ref = usePopdownCloseEvents(closeDropdownPopover)
+
+  function NavLinks() {
+    return (
+      <>
+        <span className="text-sm pt-2 pb-0 px-4 font-bold block w-full whitespace-nowrap bg-transparent text-blueGray-400"></span>
+        <Link href={routes.teams}>
+          <a
+            href="#"
+            className="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
+          >
+            BayouBowl II
+          </a>
+        </Link>
+      </>
+    )
+  }
+
   return (
-    <>
+    <div ref={ref}>
       <a
         className="lg:text-white lg:hover:text-blueGray-200 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
-        href="#pablo"
+        href="#"
         ref={btnDropdownRef}
         onClick={(e) => {
           e.preventDefault();
@@ -37,44 +52,9 @@ const TeamsDropdown = () => {
           "bg-white text-base z-50 float-left py-2 list-none text-left rounded shadow-lg min-w-48"
         }
       >
-        <span
-          className={
-            "text-sm pt-2 pb-0 px-4 font-bold block w-full whitespace-nowrap bg-transparent text-blueGray-400"
-          }
-        >
-        </span>
-        <Link href="#">
-          <a
-            href="#pablo"
-            className={
-              "text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
-            }
-            onClick={() => { alert('BayouBowl I participants are not yet available.') }}
-          >
-            BayouBowl I
-          </a>
-        </Link>
-        <Link href={routes.teams}>
-          <a
-            href="#"
-            className={
-              "text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
-            }
-          >
-            BayouBowl II
-          </a>
-        </Link>
-        <a
-          href="#"
-          className={
-            "text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
-          }
-          onClick={() => { alert('BayouBowl III participants are not yet available.') }}
-        >
-          BayouBowl III
-        </a>
+        <NavLinks />
       </div>
-    </>
+    </div>
   );
 };
 
--- components/Footers/Footer.tsx
+++ components/Footers/Footer.tsx
@@ -1,33 +1,137 @@
+import Link from 'next/link';
 import React from "react";
+import { routes } from "../../util/routes";
+
+// export default function Footer() {
+//   return (
+//     <>
+//       <footer className="relative bg-blueGray-200 pt-8 pb-6">
+//         <hr className="my-6 border-blueGray-300" />
+//         <div className="flex flex-wrap items-center md:justify-between justify-center">
+//           <div className="w-full md:w-4/12 px-4 mx-auto text-center">
+//             <div className="text-sm text-blueGray-500 font-semibold py-1">
+//               Copyright © {new Date().getFullYear()} | Built by {" "}
+//               <a
+//                 href="https://twitter.com/Mr_Spwn"
+//                 className="text-blueGray-500 hover:text-blueGray-800"
+//                 target="_blank"
+//               >
+//                 MrSpwn
+//               </a>
+//             </div>
+//             <div className="text-sm text-blueGray-500 font-semibold py-1">
+//               Not sponsored by Crytek or Hunt: Showdown.
+//             </div>
+//             <div className="text-sm text-blueGray-500 font-semibold py-1">
+//               <a
+//                 href="https://www.freeprivacypolicy.com/live/87f9752a-671b-4179-866f-654a4ef87db8"
+//                 target='_blank'
+//                 className="text-blueGray-500 hover:text-blueGray-800"
+//               >
+//                 Privacy Policy
+//               </a>
+//             </div>
+//           </div>
+//         </div>
+//       </footer>
+//     </>
+//   );
+// }
 
 export default function Footer() {
   return (
     <>
       <footer className="relative bg-blueGray-200 pt-8 pb-6">
-        <hr className="my-6 border-blueGray-300" />
-        <div className="flex flex-wrap items-center md:justify-between justify-center">
-          <div className="w-full md:w-4/12 px-4 mx-auto text-center">
-            <div className="text-sm text-blueGray-500 font-semibold py-1">
-              Copyright © {new Date().getFullYear()} | Built by {" "}
-              <a
-                href="https://twitter.com/Mr_Spwn"
-                className="text-blueGray-500 hover:text-blueGray-800"
-                target="_blank"
-              >
-                MrSpwn
-              </a>
+        <div
+          className="bottom-auto top-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden -mt-20 h-20"
+          style={{ transform: "translateZ(0)" }}
+        >
+          <svg
+            className="absolute bottom-0 overflow-hidden"
+            xmlns="http://www.w3.org/2000/svg"
+            preserveAspectRatio="none"
+            version="1.1"
+            viewBox="0 0 2560 100"
+            x="0"
+            y="0"
+          >
+            <polygon
+              className="text-blueGray-200 fill-current"
+              points="2560 0 2560 100 0 100"
+            ></polygon>
+          </svg>
+        </div>
+        <div className="container mx-auto px-4">
+          <div className="flex flex-wrap text-center lg:text-left">
+            <div className="w-full lg:w-6/12 px-4">
+              <h4 className="text-3xl font-semibold">Let's keep in touch!</h4>
+              <h5 className="text-lg mt-0 mb-2 text-blueGray-600">
+                Find us on any of these platforms.
+              </h5>
+              <div className="mt-6 lg:mb-0 mb-6">
+                <button
+                  className="bg-white text-lightBlue-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
+                  type="button"
+                >
+                  <i className="fab fa-twitter"></i>
+                </button>
+                <button
+                  className="bg-white text-purple-twitch shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
+                  type="button"
+                >
+                  <i className="fab fa-twitch"></i>
+                </button>
+              </div>
             </div>
-            <div className="text-sm text-blueGray-500 font-semibold py-1">
-              Not sponsored by Crytek or Hunt: Showdown.
+            <div className="w-full lg:w-6/12 px-4">
+              <div className="flex flex-wrap items-top mb-6">
+                <div className="w-full lg:w-4/12 px-4 ml-auto">
+                  <span className="block uppercase text-blueGray-500 text-sm font-semibold mb-2">
+                    Useful Links
+                  </span>
+                  <ul className="list-unstyled">
+                    <li>
+                      <Link href={routes.news} >
+                        <a className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm">
+                          News
+                        </a>
+                      </Link>
+                    </li>
+                    <li>
+                      <a
+                        className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+                        href="mailto:mondaynighthunts@gmail.com"
+                      >
+                        Email Us
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+                <div className="w-full lg:w-4/12 px-4">
+                  <span className="block uppercase text-blueGray-500 text-sm font-semibold mb-2">
+                    Other Resources
+                  </span>
+                  <ul className="list-unstyled">
+                    <li>
+                      <a
+                        className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+                        href="https://www.freeprivacypolicy.com/live/87f9752a-671b-4179-866f-654a4ef87db8"
+                        target="_blank"
+                      >
+                        Privacy Policy
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
             </div>
-            <div className="text-sm text-blueGray-500 font-semibold py-1">
-              <a
-                href="https://www.freeprivacypolicy.com/live/87f9752a-671b-4179-866f-654a4ef87db8"
-                target='_blank'
-                className="text-blueGray-500 hover:text-blueGray-800"
-              >
-                Privacy Policy
-              </a>
+          </div>
+          <hr className="my-6 border-blueGray-300" />
+          <div className="flex flex-wrap items-center md:justify-between justify-center">
+            <div className="w-full md:w-4/12 px-4 mx-auto text-center">
+              <div className="text-sm text-blueGray-500 font-semibold py-1">
+                Copyright © {new Date().getFullYear()} Monday Night Hunts
+              </div>
             </div>
           </div>
         </div>
