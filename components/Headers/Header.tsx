33947f76968e66071ab6bbc22f32db49a0438c94
StephenMP <1.stephen.porter@gmail.com>
2021-10-23 23:44:45 -0600

    Add watch them live integration

--- components/Headers/Header.tsx
+++ components/Headers/Header.tsx
@@ -7,6 +7,7 @@ export default function Header() {
     <>
       {/* Header */}
       <div className="relative bg-blueGray-800 md:pt-32 pb-32 pt-12"></div>
+      {process.env.NODE_ENV === 'production' && <script defer src="https://app.watchthem.live/pixel/yXsvYNKQz4XEPzHj"></script>}
     </>
   );
 }
