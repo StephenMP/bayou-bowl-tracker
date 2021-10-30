cc5451b7d8ebaafa3a9efd7dfe836714f97f184f
StephenMP <1.stephen.porter@gmail.com>
2021-10-30 13:02:57 -0600

    Update wording a bit

--- components/Footers/FooterAdmin.tsx
+++ components/Footers/FooterAdmin.tsx
@@ -10,7 +10,7 @@ export default function FooterAdmin() {
             <div className="w-full px-4 mx-auto text-center">
               <div className="text-sm text-blueGray-500 font-semibold py-1 text-center md:text-left">
                 * By registering to compete, you certify that you are of at least 13 years of age, or the legal minimum
-                age required in your country to compete in a competitive tournament with a cash prize, and that if
+                age required in your state or country to compete in a competitive tournament with a cash prize, and that if
                 legally considered a minor, have the permission of your parent or legal guardian to compete in The Bayou Bowl III
                 tournament. You also certify that you agree to our{' '}
                 <a className="font-bold" href="/pdf/Bayou_Bowl_III_Rules.pdf" target="_blank" rel="noopener noreferrer">
